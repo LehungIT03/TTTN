f7ffdd06b6727267b462e6cf927603621d84b514
MSI\hung1 <hung1456320@gmail.com>
2025-04-12 19:27:32 +0700

    no message

---
+++ etailcar/123/server/controllers/cars.js
@@ -0,0 +1,91 @@
+import { CarModel } from "../models/CarModel.js";
+import { ContractModel } from "../models/ContractModel.js";
+import { DetailsContractModel } from "../models/DetailsContractModel.js";
+
+export const getCars = async (req, res) => {
+  try {
+    const cars = await CarModel.find();
+    res.status(200).json(cars);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+};
+
+export const getCar = async (req, res) => {
+  try {
+    const { _id } = req.body; // Lấy ID của xe từ request body
+
+    // Tìm thông tin xe dựa trên _id
+    const car = await CarModel.findOne({ _id });
+
+    if (!car) {
+      return res.status(404).json({ message: "Car not found" });
+    }
+
+    // Tìm tất cả hợp đồng liên quan đến chiếc xe này từ DetailsContractModel
+    const carContracts = await DetailsContractModel.find({
+      carid: _id, // Kiểm tra tất cả hợp đồng liên quan đến xe này
+    });
+
+    let isRented = false;
+    // Duyệt qua tất cả các hợp đồng và kiểm tra xem có hợp đồng nào có status là 'Lấy xe' không
+    for (const detailContract of carContracts) {
+      const contract = await ContractModel.findOne({
+        _id: detailContract.contractid,
+        status: "Đã đặt cọc", // Kiểm tra hợp đồng có trạng thái 'Lấy xe'
+      });
+      console.log(contract);
+      if (contract) {
+        isRented = true;
+        break; // Nếu tìm thấy một hợp đồng đang lấy xe, không cần kiểm tra tiếp
+      }
+    }
+
+    // Trả về thông tin xe và trạng thái của việc thuê xe
+    res.status(200).json({
+      car,
+      isRented,
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+export const createCar = async (req, res) => {
+  try {
+    const newcar = req.body;
+    const car = new CarModel(newcar);
+
+    await car.save();
+
+    res.status(200).json(car);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+};
+
+export const updateCar = async (req, res) => {
+  try {
+    const updateCar = req.body;
+    const car = await CarModel.findOneAndUpdate(
+      { _id: updateCar._id },
+      updateCar.carData,
+      { new: true }
+    );
+    console.log("[car]", car);
+    res.status(200).json(car);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+};
+
+export const removeCar = async (req, res) => {
+  try {
+    const updateCar = req.body;
+
+    const car = await CarModel.findByIdAndDelete({ _id: updateCar._id });
+    console.log("[car]", car);
+    res.status(200).json(car);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+};
---
+++ etailcar/123/server/index.js
@@ -0,0 +1,88 @@
+import cors from "cors";
+import express from "express";
+import mongoose from "mongoose";
+import { CarModel } from "./models/CarModel.js";
+import data from "./rentcar.json" with { type: "json" };
+import cars from "./routers/cars.js";
+import contracts from "./routers/contracts.js";
+import detailscontracts from "./routers/detailscontracts.js";
+import posts from "./routers/posts.js";
+import reviews from "./routers/reviews.js";
+import users from "./routers/users.js";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+// const URI = 'mongodb://localhost:27017/rentcar';
+const URI =
+  "mongodb+srv://hung:admin@cluster0.iir89ee.mongodb.net/TTTN?retryWrites=true&w=majority&appName=Cluster0";
+
+app.use(cors());
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ extended: true, limit: "100mb" }));
+
+app.use("/posts", posts);
+app.use("/users", users);
+app.use("/cars", cars);
+app.use("/contracts", contracts);
+app.use("/detailscontracts", detailscontracts);
+app.use("/reviews", reviews);
+
+const importData = async () => {
+  try {
+    const count = await CarModel.countDocuments();
+    if (count === 0) {
+      let formattedData = [];
+      for (let i = 0; i < data.length; i++) {
+        const createdAt = new Date();
+        const updatedAt = new Date();
+        console.log();
+
+        // Kiểm tra xem có phải là một ngày hợp lệ không
+        if (isNaN(createdAt) || isNaN(updatedAt)) {
+          continue; // Bỏ qua mục này và tiếp tục với mục tiếp theo
+        }
+        console.log(data[i].carname);
+        formattedData.push({
+          carname: data[i].carname,
+          pricerent: data[i].pricerent,
+          cartype: data[i].cartype,
+          carcompany: data[i].carcompany,
+          image: data[i].image,
+          description: data[i].description,
+          createdAt,
+          updatedAt,
+        });
+      }
+
+      // Chỉ chèn vào cơ sở dữ liệu nếu dữ liệu hợp lệ
+      if (formattedData.length > 0) {
+        await CarModel.insertMany(formattedData);
+        console.log("Data imported successfully");
+      } else {
+        console.log("No valid data to import");
+      }
+    } else {
+      console.log("Data already exists");
+    }
+  } catch (error) {
+    console.error("Error importing data:", error);
+  }
+};
+
+const start = async () => {
+  await importData();
+};
+
+start();
+
+mongoose
+  .connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to DB");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Error connecting to DB:", err);
+  });
